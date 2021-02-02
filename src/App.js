5468e4ef33dfca51779b4bd88c5a768b8b1d8d14
shva <shova.shrestha@globaly.io>
2021-02-02 21:59:54 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import MessageList from './message-list';
+
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <MessageList />
+    )
+  }
+}
+
+export default App;
---
+++ src/message-view.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MessageView extends React.Component {
+  render() {
+    const message = this.props.message;
+
+    return(
+      <div className="message">
+        <div className="field">
+          <span className="label">From: </span>
+          <span className="value">{message.from}</span>
+        </div>
+        <div className="field">
+          <span className="label">Status: </span>
+          <span className="value">{message.status}</span>
+        </div>
+        <div className="field content">
+          <span className="label">Message: </span>
+          <span className="value">{message.content}</span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MessageView;
